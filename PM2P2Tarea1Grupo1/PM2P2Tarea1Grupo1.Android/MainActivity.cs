5f2be5da9c5dbef38afc46c307650b2afb6bacfd
gereld10 <gereldamids42@gmail.com>
2023-07-17 23:42:57 -0600

    fILE

--- PM2P2Tarea1Grupo1/PM2P2Tarea1Grupo1.Android/MainActivity.cs
+++ PM2P2Tarea1Grupo1/PM2P2Tarea1Grupo1.Android/MainActivity.cs
@@ -15,7 +15,7 @@ namespace PM2P2Tarea1Grupo1.Droid
            // UserDialogs.Init(this);
             base.OnCreate(savedInstanceState);
 
-            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
+          //  Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
@@ -24,10 +24,7 @@ namespace PM2P2Tarea1Grupo1.Droid
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            Plugin.Permissions.
-
-
-
+          //  Plugin.Permissions.PermissionsImplementatio.Current.OnRequestPermissionsResult(requestCode,permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
