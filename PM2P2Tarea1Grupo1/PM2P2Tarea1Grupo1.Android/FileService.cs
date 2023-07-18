8e78adde40d39490abf970775adffdc4b6a06a93
Nezca17 <126973834+Nezca17@users.noreply.github.com>
2023-07-18 07:10:05 -0600

    probado interfaz para guardar archivos

---
+++ PM2P2Tarea1Grupo1/PM2P2Tarea1Grupo1.Android/FileService.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using PM2P2Tarea1Grupo1.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PM2P2Tarea1Grupo1.Droid
+{
+    public class FileService : IFileService
+    {
+
+        public string GetRootPath()
+        {
+            return Application.Context.GetExternalFilesDir(null).ToString();
+        }
+
+        public void CreateFile(String video)
+        {
+            var filename = "text.txt";
+            var destination = Path.Combine(GetRootPath(), filename);
+
+            File.WriteAllText(filename, video);
+
+        }
+    }
+}
---
+++ PM2P2Tarea1Grupo1/PM2P2Tarea1Grupo1/Interface/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM2P2Tarea1Grupo1.Interface
+{
+   public interface IFileService
+    {
+        void CreateFile(string Video);
+    }
+}
