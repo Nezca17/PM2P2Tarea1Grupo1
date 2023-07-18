43682ed4d8e5b25c9a80eadb84577b6be797b3a0
gereld10 <gereldamids42@gmail.com>
2023-07-18 11:05:06 -0600

    MediaElement agregado

--- PM2P2Tarea1Grupo1/PM2P2Tarea1Grupo1.Android/MainActivity.cs
+++ PM2P2Tarea1Grupo1/PM2P2Tarea1Grupo1.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Runtime;
 using Android.OS;
 using Acr.UserDialogs;
 using Plugin.Media;
-using Plugin.Permissions;
+
 
 
 namespace PM2P2Tarea1Grupo1.Droid
@@ -18,7 +18,6 @@ namespace PM2P2Tarea1Grupo1.Droid
            UserDialogs.Init(this);
             base.OnCreate(savedInstanceState);
 
-          Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
@@ -26,9 +25,8 @@ namespace PM2P2Tarea1Grupo1.Droid
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
